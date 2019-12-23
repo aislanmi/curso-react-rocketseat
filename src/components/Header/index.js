f9159eaae3e9ecc27b85c648ea7d41f418571378
Aislan Michel Moreira Freitas <aislan.michel92@gmail.com>
2019-12-23 20:13:03 -0300

    First commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './styles.css'
+
+const Header = () => (
+    <header id='main-header'>JSHunt</header>
+
+)
+
+//https://rocketseat-node.herokuapp.com/api
+
+export default Header
